db454ddbd7efcc774a3c46b3901a8750b37064bd
charalambosp <papaloizouc@gmail.com>
2014-02-11 22:56:54 +0000

    delete auto generated staff...

--- ReneMagritte/src/main/java/org/renemagritte/Foo.java
+++ ReneMagritte/src/main/java/org/renemagritte/Foo.java
@@ -1,21 +1,9 @@
 package org.renemagritte;
 
-import org.renemagritte.util.SystemUiHider;
-
-import android.annotation.TargetApi;
 import android.app.Activity;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.MotionEvent;
-import android.view.View;
 
-/**
- * An example full-screen activity that shows and hides the system UI (i.e.
- * status bar and navigation/system bar) with user interaction.
- *
- * @see SystemUiHider
- */
+
 public class Foo extends Activity {
 
     @Override
